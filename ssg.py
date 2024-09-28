20b65fa25e3154d4a2fdd615492ca974cc34358b
Will Shick <wshick.professional@gmail.com>
2024-09-28 19:26:18 -0400

    extensions

--- ssg.py
+++ ssg.py
@@ -1,7 +1,8 @@
 from shutil import copytree
 
 import markdown
-extensions = ['extra', 'admonition', 'meta', 'nl2br', 'sane_lists', 'smarty', 'wikilinks']
+official_extensions = ['extra', 'admonition', 'meta', 'nl2br', 'smarty']
+thirdparty_extensions = ['markdown_sub_sup', 'markdown-del-ins', 'markdown_mark', 'markdown-gfm-admonition', 'mdx_outline', 'mdx_truly_sane_lists', 'mdx_wikilink_plus', 'python-tableau-parser']
 
 # Copy static files to the build directory
 copytree("static/", "build/", dirs_exist_ok=True)
@@ -17,7 +18,7 @@ with open("content/index.md") as file:
 	content = file.read()
 
 # Convert the markdown to HTML
-html = markdown.markdown(content, extensions=extensions)
+html = markdown.markdown(content, extensions=[official_extensions, thirdparty_extensions])
 
 # Insert the HTML content into the template
 output: str = template.replace("{{content}}", html)
